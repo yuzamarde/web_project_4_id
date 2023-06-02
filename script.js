188c02b462a8affad953a2fc4db4f5abe571664d
yuzamarde <yuzamarde@gmail.com>
2023-06-02 17:28:38 +0700

    intial commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openForm() {
     var popup = document.getElementsByClassName("form")[0];
     popup.style.display = "block";
-  }
+}
 
-  function submitForm(event) {
+function submitForm(event) {
     event.preventDefault(); // Prevent form submission
     var newName = document.getElementsByClassName("form__field-1")[0].value;
     var newSubName = document.getElementsByClassName("form__field-2")[0].value;
@@ -15,21 +15,21 @@ function openForm() {
     subProfileName.textContent = newSubName;
 
     closeForm();
-  }
+}
 
-  function closeForm() {
+function closeForm() {
     var popup = document.getElementsByClassName("form")[0];
     popup.style.display = "none";
   }
 
   // Get the form element by class name
-  var formElement = document.getElementsByClassName('form__open')[0].querySelector('form');
+var formElement = document.getElementsByClassName('form__open')[0].querySelector('form');
   // Get the name input element by class name
-  var nameInput = document.getElementsByClassName('field-1')[0];
+var nameInput = document.getElementsByClassName('field-1')[0];
   // Get the close button by class name
-  var closeButton = document.getElementsByClassName('close')[0];
+var closeButton = document.getElementsByClassName('close')[0];
 
   // Add event listener to form submission
-  formElement.addEventListener('submit', submitForm);
+formElement.addEventListener('submit', submitForm);
   // Add event listener to close button
-  closeButton.addEventListener('click', closeForm);
+closeButton.addEventListener('click', closeForm);
