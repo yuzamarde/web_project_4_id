6d32cea278dbea51a88fc969798c9ee23106c3fe
yuzamarde <yuzamarde@gmail.com>
2023-06-03 15:41:04 +0700

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const tombol = document.querySelector('.profile__profile-info_edit-btn');
-const close = document.querySelector('.form__close');
-const save = document.querySelector('.form__submit-button');
-const box = document.querySelector('.form');
-const input = document.querySelector('.form__field-1');
-const subtextInput = document.querySelector('.form__field-2');
+const close = document.querySelector('.edit-form__close');
+const save = document.querySelector('.edit-form__profil-submit-button');
+const box = document.querySelector('.edit-form');
+const input = document.querySelector('.edit-form__profil-field-1');
+const subtextInput = document.querySelector('.edit-form__profil-field-2');
 const text = document.querySelector('.profile__profile-info_title');
 const subtext = document.querySelector('.profile__profile-info_subtitle');
 
@@ -29,3 +29,21 @@ function foo(selector) {
 }
 foo('.text');
 foo('.subtext');
+
+// Select all elements with the class "element__group"
+var btns = document.querySelectorAll(".element__group");
+
+// Iterate over each element
+btns.forEach(function(btn) {
+  // Find the icon element within the current element
+  var icon = btn.querySelector(".element__button");
+
+  // Attach the click event handler to the current element
+  btn.onclick = function() {
+    if (icon.classList.contains("element__button")) {
+      icon.classList.replace("element__button", "element__button-love");
+    } else {
+      icon.classList.replace("element__button-love", "element__button");
+    }
+  };
+});
