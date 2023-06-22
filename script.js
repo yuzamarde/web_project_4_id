261f23d52dedd0546edc9582cfd01eee13c1d987
yuzamarde <yuzamarde@gmail.com>
2023-06-22 16:15:30 +0700

    initial commit

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ const authorName = document.querySelector('.profile__name');
 const authorWork = document.querySelector('.profile__work');
 const editAuthorPopupForm = document.querySelector('.popup__form');
 
-
-// popup edit name
 editProfileButton.addEventListener('click', () => {
   editPopupBox.classList.add('popup_active');
   authorNewName.value = authorName.textContent;
@@ -27,8 +25,7 @@ closePopupButton.addEventListener('click', () => {
   editPopupBox.classList.remove('popup_active');
 });
 
-
-// templete
+// template
 const template = document.querySelector('.elements');
 const container = document.querySelector('.container');
 const initialCards = [
@@ -68,21 +65,41 @@ function activeLike(evt) {
   evt.target.classList.toggle('element__button-active');
 }
 
-for (let x = 0; x < 6; x++) {
+function deleteElement() {
+  const element = this.parentElement;
+  element.remove();
+}
+
+function createCard(cardData) {
   const clone = template.content.cloneNode(true);
-  // template diinject ke dalam container
   const cardImage = clone.querySelector('.element__image');
   const cardTitle = clone.querySelector('.element__title');
   const cardButton = clone.querySelector('.element__button');
-  //inject initialCards
-  cardImage.src = initialCards[x].link;
-  cardTitle.innerHTML = initialCards[x].name;
-  // pasang event listener
+  const deleteButton = clone.querySelector('.element__delete');
+
+  cardImage.src = cardData.link;
+  cardTitle.innerHTML = cardData.name;
+
   cardButton.addEventListener('click', selectPicture);
   cardButton.addEventListener('click', activeLike);
-  // memindahkan tempalte ke container
+  cardImage.addEventListener('click', () => {
+    popupImageFile.src = cardData.link;
+    popupImageTitle.textContent = cardData.name;
+    popupImage.classList.add('zoom_active');
+  });
+  deleteButton.addEventListener('click', deleteElement);
+
+  return clone;
+}
+
+function addCardToContainer(cardData) {
+  const newCard = createCard(cardData);
   const firstElement = container.firstChild;
-  container.insertBefore(clone, firstElement);
+  container.insertBefore(newCard, firstElement);
+}
+
+for (let x = 0; x < initialCards.length; x++) {
+  addCardToContainer(initialCards[x]);
 }
 
 // portfolio item add
@@ -95,113 +112,101 @@ const portfolioTitle = document.querySelector('input[name="form__input-name"]');
 const portfolioForm = document.querySelector('.forms__form');
 const submitButton = document.querySelector('.forms__submit');
 
-// Function to check if both form inputs are filled
 function checkFormInputs() {
-  const urlValue = portfolioImage.value.trim();
-  const nameValue = portfolioTitle.value.trim();
-  
-  if (urlValue !== '' && nameValue !== '') {
-    submitButton.classList.add('forms__submit-active');
-  } else {
-    submitButton.classList.remove('forms__submit-active');
+    const urlValue = portfolioImage.value.trim();
+    const nameValue = portfolioTitle.value.trim();
+    
+    if (urlValue !== '' && nameValue !== '') {
+      submitButton.classList.add('forms__submit-active');
+    } else {
+      submitButton.classList.remove('forms__submit-active');
+    }
+    portfolioForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      portfolioImage.textContent = '';
+      portfolioTitle.textContent = '';
+      submitButton.classList.remove('forms__submit-active');
+    });
   }
+
   portfolioForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    portfolioImage.textContent = '';
-    portfolioTitle.textContent = '';
+  //   // Get the input values
+    const urlValue = portfolioImage.value.trim();
+    const nameValue = portfolioTitle.value.trim();
     submitButton.classList.remove('forms__submit-active');
+    
+    // Display the input values
+    portfolioImage.textContent = urlValue;
+    portfolioTitle.textContent = nameValue;
   });
-}
+  
+  
+  // Add event listeners to the input fields
+  portfolioImage.addEventListener('input', checkFormInputs);
+  portfolioTitle.addEventListener('input', checkFormInputs);
+  
+  
+  portfolioAddButton.addEventListener('click', () => {
+    portfolioPopupBox.classList.add('forms_active');
+  });
+  
+  const errImg = document.querySelector('.forms__error-img');
+  const errMsg = document.querySelector('.forms__error-msg');
 
-portfolioForm.addEventListener('submit', (event) => {
+
+  
+
+portfolioForm.addEventListener('submit', function(event) {
   event.preventDefault();
-  // Get the input values
+
+  console.log(
+    portfolioImage.value,
+    portfolioTitle.value,
+)
+
+errImg.textContent = '';
+errMsg.textContent = '';
+
   const urlValue = portfolioImage.value.trim();
   const nameValue = portfolioTitle.value.trim();
-  submitButton.classList.remove('forms__submit-active');
+
+  if (portfolioImage.value.trim() === '' && portfolioTitle.value.trim() === '') {
+    errImg.textContent = 'Image Url harus diisi!';
+    errMsg.textContent = 'Title harus diisi!';
+    return false;
+}
+
+if (portfolioImage.value.trim() === '') {
+    errImg.classList.add('forms__error-img--active');
+    errImg.textContent = 'Image Url harus diisi!';
+    return false;
+}
+if (portfolioTitle.value.trim() === '') {
+    errMsg.classList.add('forms__error-msg--active');
+    errMsg.textContent = 'Title harus diisi!';
+    return false;
+}
+  portfolioPopupBox.classList.remove('forms_active');
+
+  addCardToContainer({ name: nameValue, link: urlValue });
   
-  // Display the input values
-  portfolioImage.textContent = urlValue;
-  portfolioTitle.textContent = nameValue;
+  // remove class
+  errMsg.classList.remove('forms__error-msg--active')
+  errImg.classList.remove('forms__error-img--active')
+  
+  portfolioImage.value = '';
+  portfolioTitle.value = '';
 });
-
-
-// Add event listeners to the input fields
-portfolioImage.addEventListener('input', checkFormInputs);
-portfolioTitle.addEventListener('input', checkFormInputs);
-
 
 portfolioAddButton.addEventListener('click', () => {
   portfolioPopupBox.classList.add('forms_active');
 });
 
-const errImg = document.querySelector('.forms__error-img');
-const errMsg = document.querySelector('.forms__error-msg');
-
-
-
-portfolioForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  
-
-  console.log(
-      portfolioImage.value,
-      portfolioTitle.value,
-  )
-
-  errImg.textContent = '';
-  errMsg.textContent = '';
-  
-
-  // logic error missage
-  if (portfolioImage.value.trim() === '' && portfolioTitle.value.trim() === '') {
-      errImg.textContent = 'Image Url harus diisi!';
-      errMsg.textContent = 'Title harus diisi!';
-      return false;
-  }
-
-  if (portfolioImage.value.trim() === '') {
-      errImg.classList.add('forms__error-img--active');
-      errImg.textContent = 'Image Url harus diisi!';
-      return false;
-  }
-  if (portfolioTitle.value.trim() === '') {
-      errMsg.classList.add('forms__error-msg--active');
-      errMsg.textContent = 'Title harus diisi!';
-      return false;
-  }
-  portfolioPopupBox.classList.remove('forms_active');
-  
-  const clone = template.content.cloneNode(true);
-
-  const contentImage = clone.querySelector('.element__image');
-  const contentTitle = clone.querySelector('.element__title');
-
-  // memamasukkan nilai ke title
-  contentImage.src = portfolioImage.value;
-  contentTitle.textContent = portfolioTitle.value;
-
-  // dorong template ke container
-  const firstElement = container.firstChild;
-  container.insertBefore(clone, firstElement);
-
-
-  // remove class
-  errMsg.classList.remove('forms__error-msg--active')
-  errImg.classList.remove('forms__error-img--active')
-  
-  // clean up value
-  portfolioImage.value = '';
-  portfolioTitle.value = '';
-
-});
-// remove popup-form
 portfolioCloseButton.addEventListener('click', (event) => {
   event.preventDefault();
   portfolioPopupBox.classList.remove('forms_active');
 });
-
-
 
 // image popup
 const elements = document.querySelectorAll('.element');
@@ -210,7 +215,7 @@ const popupImageFile = document.querySelector('.zoom__file');
 const popupImageTitle = document.querySelector('.zoom__title');
 const popupImageClose = document.querySelector('.zoom__close');
 
-// Event listener for each element
+
 elements.forEach(element => {
   const image = element.querySelector('.element__image');
   const title = element.querySelector('.element__title');
@@ -222,20 +227,13 @@ elements.forEach(element => {
   });
 });
 
-// Event listener for closing the popup image
 popupImageClose.addEventListener('click', () => {
   popupImage.classList.remove('zoom_active');
 });
 
-
-
-// delete element
+// Delete element
 const deleteButtons = document.querySelectorAll('.element__delete');
 
-// Add click event listener to each delete button
 deleteButtons.forEach((deleteButton) => {
-  deleteButton.addEventListener('click', () => {
-    const element = deleteButton.parentElement;
-    element.remove();
-  });
+  deleteButton.addEventListener('click', deleteElement);
 });
