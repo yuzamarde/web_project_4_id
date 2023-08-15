8cbd64b7d3d99fe14c7c4b2e21ec61c457e55d3e
yuzamarde <yuzamarde@gmail.com>
2023-08-15 10:22:10 +0700

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   devtool: 'inline-source-map',
   entry: {
-    main: "./src/pages/index.js"
+    main: "./src/index.js"
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -20,7 +20,7 @@ module.exports = {
   devServer: {
     static: path.resolve(__dirname, "./dist"),
     compress: true,
-    port: 8081,
+    port: 8080,
     open: true
   },
   module: {
